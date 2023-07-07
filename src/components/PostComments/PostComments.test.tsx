7598a39e25bc1b9ed76ecf6af259379afda97358
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-07-06 22:59:03 -0300

    adicionando teste rtl

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it ('Deve adicionar 2 comentários', () => {
+        render(<PostComment />);
+
+        const button = screen.getByTestId('btn-comment');
+        const comment = screen.getByTestId('txt-comment')
+
+        fireEvent.change(comment, {
+            target: {
+                value: 'Comentário 1',
+            }
+        });
+        fireEvent.click(button);
+
+        fireEvent.change(comment, {
+            target: {
+                value: 'Comentário 2',
+            }
+        });
+        fireEvent.click(button);
+
+        const result = screen.getAllByTestId('item-coment');
+        expect(result).toHaveLength(2);
     });
 });
